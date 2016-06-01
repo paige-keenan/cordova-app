f85e76556911409c46a8e053b88f8db9158037a9
Paige Keenan <paikeena@usatlrad3007.global.publicisgroupe.net>
2016-06-01 09:56:51 -0400

    init app setup: angular integration, camera integration, practice examples

--- MyApp/www/js/app.js
+++ MyApp/www/js/app.js
@@ -1,17 +1,94 @@
+  // create the module and name it myApp
+  var myApp = angular.module('myApp', ['ionic', 'ngCordova', 'ui.router']);
 
-//setup angular
-var app = angular.module('myApp', ['ionic']);
+  myApp.config(function($stateProvider, $urlRouterProvider) {
 
-app.controller('main', function ($scope) {
+    $urlRouterProvider.otherwise('/home');
 
-  //initialize the tasks scope with empty array
-  $scope.tasks = [];
+    $stateProvider
 
-  //initialize the task scope with empty object
-  $scope.task = {};
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'pages/home/home.html'
+        })
 
-  $scope.getTasks = function () {
-    console.log('app loaded');
-  }
+        .state('home.list', {
+            url: '/list',
+            templateUrl: 'pages/home/list.html',
+            controller: function($scope) {
+                $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+            }
+        })
 
-});
+        // nested list with just some random string data
+        .state('home.paragraph', {
+            url: '/paragraph',
+            template: 'I could sure use a drink right now.'
+        })
+
+
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+            url: '/about',
+            views: {
+
+                // the main template will be placed here (relatively named)
+                '': { templateUrl: 'pages/about/about.html' },
+
+                // the child views will be defined here (absolutely named)
+                'columnOne@about': { template: 'Look I am a column!' },
+
+                // for column two, we'll define a separate controller
+                'columnTwo@about': {
+                    templateUrl: 'pages/table-data.html',
+                    controller: 'itemsController'
+                }
+            }
+
+        });
+
+    });
+
+    myApp.controller('itemsController', function($scope) {
+
+        $scope.message = 'test';
+
+        $scope.scotches = [
+            {
+                name: 'Macallan 12',
+                price: 50
+            },
+            {
+                name: 'Chivas Regal Royal Salute',
+                price: 10000
+            },
+            {
+                name: 'Glenfiddich 1937',
+                price: 20000
+            }
+        ];
+
+    });
+
+myApp.controller('profileEditCtrl', function($scope,Camera, $localStorage,
+ $cordovaCamera)
+     {
+        $scope.$storage = $localStorage.$default({ data:[]});
+
+    $scope.takePicture = function()
+    {
+        navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+            destinationType: Camera.DestinationType.DATA_URL });
+
+      function onSuccess(imageData) {
+        var image = document.getElementById('myImage');
+        image.src ="data:image/jpeg;base64," + imageData;
+    }
+
+    function onFail(message) {
+        alert('Failed because: ' + message);
+    }
+
+}});
+
