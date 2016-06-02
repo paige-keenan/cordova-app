25393975b948320e1c4c8f74ff07a0fa0cba8ab5
Paige Keenan <paikeena@usatlrad3007.global.publicisgroupe.net>
2016-06-02 18:40:14 -0400

    updated images

--- MyApp/www/js/app.js
+++ MyApp/www/js/app.js
@@ -1,101 +1,100 @@
-  // create the module and name it myApp
-  var myApp = angular.module('myApp', ['ionic', 'ngCordova', 'ui.router'])
+// create the module and name it myApp
+var myApp = angular.module('myApp', ['ionic', 'ngCordova', 'ui.router'])
 
-  myApp.config(function($stateProvider, $urlRouterProvider) {
+myApp.config(function($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise('/home');
+  $urlRouterProvider.otherwise('/home');
 
-    $stateProvider
+  $stateProvider
 
-        // HOME STATES AND NESTED VIEWS ========================================
-        .state('home', {
-            url: '/home',
-            templateUrl: 'pages/home/home.html',
-            controller: 'itemsController'
-        })
+    // HOME STATES AND NESTED VIEWS ========================================
+    .state('home', {
+      url: '/home',
+      templateUrl: 'pages/home/home.html',
+      controller: 'homeController'
+    })
 
 
-        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
-        .state('about', {
-            url: '/about',
-            templateUrl: 'pages/table-data.html',
-            controller: 'aboutController'
-
-        });
-
+    // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+    .state('about', {
+      url: '/about',
+      templateUrl: 'pages/about/about.html',
+      controller: 'aboutController'
     });
 
-    myApp.controller('aboutController', function($scope) {
-      $scope.imageTextModulesAbout = [
-        {
-          img: '../../img/home-hero.png',
-          imgAlt: 'Lorem Ipsum',
-          headline: 'Paige',
-          body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to.',
-          buttonUrl: '#',
-          buttonText: 'Learn More'
-        },
-        {
-          img: '../../img/selfie-sundae-hero.png',
-          imgAlt: 'Lorem Ipsum',
-          headline: 'Selfie Sundaes',
-          body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to.',
-          buttonUrl: '#',
-          buttonText: 'Learn More'
-        },
-        {
-          img: '../../img/home-hero.png',
-          imgAlt: 'Lorem Ipsum',
-          headline: 'Iam & absurdum',
-          body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to.',
-          buttonUrl: '#',
-          buttonText: 'Learn More'
-        }
-      ];
-    });
+});
 
-    myApp.controller('itemsController', function($scope) {
-      $scope.imageTextModulesHome = [
-        {
-          img: '../../img/home-hero.png',
-          imgAlt: 'Lorem Ipsum',
-          headline: 'New Flavor Alert',
-          body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to.',
-          buttonUrl: '#',
-          buttonText: 'Learn More'
-        },
-        {
-          img: '../../img/selfie-sundae-hero.png',
-          imgAlt: 'Lorem Ipsum',
-          headline: 'Selfie Sundaes',
-          body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to.',
-          buttonUrl: '#',
-          buttonText: 'Learn More'
-        },
-        {
-          img: '../../img/summer-flavors-hero.jpg',
-          imgAlt: 'Lorem Ipsum',
-          headline: 'Try Our Summer Flavors',
-          body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to.',
-          buttonUrl: '#',
-          buttonText: 'Learn More'
-        },
-        {
-          img: '../../img/swag-hero.jpg',
-          imgAlt: 'Lorem Ipsum',
-          headline: 'Swag Shop',
-          body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to.',
-          buttonUrl: '#',
-          buttonText: 'Shop Now'
-        }
-      ];
-    });
+myApp.controller('homeController', function($scope) {
+  $scope.imageTextModulesHome = [
+    {
+      img: '../../img/home/home-hero.png',
+      imgAlt: 'Lorem Ipsum',
+      headline: 'New Flavor Alert',
+      body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to.',
+      buttonUrl: '#',
+      buttonText: 'Learn More'
+    },
+    {
+      img: '../../img/home/selfie-sundae-hero.png',
+      imgAlt: 'Lorem Ipsum',
+      headline: 'Selfie Sundaes',
+      body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to.',
+      buttonUrl: '#',
+      buttonText: 'Learn More'
+    },
+    {
+      img: '../../img/home/summer-flavors-hero.jpg',
+      imgAlt: 'Lorem Ipsum',
+      headline: 'Try Our Summer Flavors',
+      body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to.',
+      buttonUrl: '#',
+      buttonText: 'Learn More'
+    },
+    {
+      img: '../../img/home/swag-hero.jpg',
+      imgAlt: 'Lorem Ipsum',
+      headline: 'Swag Shop',
+      body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to.',
+      buttonUrl: '#',
+      buttonText: 'Shop Now'
+    }
+  ];
+});
 
-    myApp.controller('navControl', function($scope) {
-      $scope.isActive = false;
-      $scope.toggleNav = function(scope) {
-          $scope.isActive = !$scope.isActive;
-      };
-    });
+myApp.controller('aboutController', function($scope) {
+  $scope.imageTextModulesAbout = [
+    {
+      img: '../../img/home/home-hero.png',
+      imgAlt: 'Lorem Ipsum',
+      headline: 'Paige',
+      body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to.',
+      buttonUrl: '#',
+      buttonText: 'Learn More'
+    },
+    {
+      img: '../../img/home/selfie-sundae-hero.png',
+      imgAlt: 'Lorem Ipsum',
+      headline: 'Selfie Sundaes',
+      body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to.',
+      buttonUrl: '#',
+      buttonText: 'Learn More'
+    },
+    {
+      img: '../../img/home/home-hero.png',
+      imgAlt: 'Lorem Ipsum',
+      headline: 'Iam & absurdum',
+      body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to.',
+      buttonUrl: '#',
+      buttonText: 'Learn More'
+    }
+  ];
+});
+
+myApp.controller('navControl', function($scope) {
+  $scope.isActive = false;
+  $scope.toggleNav = function(scope) {
+      $scope.isActive = !$scope.isActive;
+  };
+});
 
 
