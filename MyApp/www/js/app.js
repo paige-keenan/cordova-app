44f0eb8dfbba3ee9315105d479316e051cd95a71
Paige Keenan <paikeena@usatlrad3007.global.publicisgroupe.net>
2016-06-02 16:29:29 -0400

    more homepage styling, remove unused code from examples

--- MyApp/www/js/app.js
+++ MyApp/www/js/app.js
@@ -10,57 +10,83 @@
         // HOME STATES AND NESTED VIEWS ========================================
         .state('home', {
             url: '/home',
-            templateUrl: 'pages/home/home.html'
+            templateUrl: 'pages/home/home.html',
+            controller: 'itemsController'
         })
 
 
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
         .state('about', {
             url: '/about',
-            views: {
-
-                // the main template will be placed here (relatively named)
-                '': { templateUrl: 'pages/about/about.html' },
-
-                // the child views will be defined here (absolutely named)
-                'columnOne@about': { template: 'Look I am a column!' },
-
-                // for column two, we'll define a separate controller
-                'columnTwo@about': {
-                    templateUrl: 'pages/table-data.html',
-                    controller: 'itemsController'
-                }
-            }
+            templateUrl: 'pages/table-data.html',
+            controller: 'aboutController'
 
         });
 
     });
 
+    myApp.controller('aboutController', function($scope) {
+      $scope.imageTextModulesAbout = [
+        {
+          img: '../../img/home-hero.png',
+          imgAlt: 'Lorem Ipsum',
+          headline: 'Paige',
+          body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to.',
+          buttonUrl: '#',
+          buttonText: 'Learn More'
+        },
+        {
+          img: '../../img/selfie-sundae-hero.png',
+          imgAlt: 'Lorem Ipsum',
+          headline: 'Selfie Sundaes',
+          body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to.',
+          buttonUrl: '#',
+          buttonText: 'Learn More'
+        },
+        {
+          img: '../../img/home-hero.png',
+          imgAlt: 'Lorem Ipsum',
+          headline: 'Iam & absurdum',
+          body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to.',
+          buttonUrl: '#',
+          buttonText: 'Learn More'
+        }
+      ];
+    });
+
     myApp.controller('itemsController', function($scope) {
-
-        $scope.message = 'test';
-
-        $scope.scotches = [
-            {
-                name: 'Macallan 12',
-                price: 50
-            },
-            {
-                name: 'Chivas Regal Royal Salute',
-                price: 10000
-            },
-            {
-                name: 'Glenfiddich 1937',
-                price: 20000
-            }
-        ];
-
+      $scope.imageTextModulesHome = [
+        {
+          img: '../../img/home-hero.png',
+          imgAlt: 'Lorem Ipsum',
+          headline: 'New Flavor',
+          body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to.',
+          buttonUrl: '#',
+          buttonText: 'Learn More'
+        },
+        {
+          img: '../../img/selfie-sundae-hero.png',
+          imgAlt: 'Lorem Ipsum',
+          headline: 'Selfie Sundaes',
+          body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to.',
+          buttonUrl: '#',
+          buttonText: 'Learn More'
+        },
+        {
+          img: '../../img/summer-flavors-hero.png',
+          imgAlt: 'Lorem Ipsum',
+          headline: 'Try Our Summer Flavors',
+          body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to.',
+          buttonUrl: '#',
+          buttonText: 'Learn More'
+        }
+      ];
     });
 
     myApp.controller('navControl', function($scope) {
-      $scope.isActive = false
-      $scope.toggleNav = function() {
-          $scope.isActive = !$scope.isActive
+      $scope.isActive = false;
+      $scope.toggleNav = function(scope) {
+          $scope.isActive = !$scope.isActive;
       };
     });
 
