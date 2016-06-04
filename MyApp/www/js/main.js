4e723013c9b2ac173f98a9c27e438ec501ffb7d9
Paige Keenan <paikeena@usatlrad3007.global.publicisgroupe.net>
2016-06-03 22:09:31 -0400

    ios bug fix and dependencies added

--- MyApp/www/js/main.js
+++ MyApp/www/js/main.js
@@ -1,28 +1,26 @@
-(function() {
+var pictureSource;   // picture source
+var destinationType; // sets the format of returned value
 
-  var pictureSource;   // picture source
-  var destinationType; // sets the format of returned value
+document.addEventListener("deviceready",onDeviceReady,false);
 
-  document.addEventListener("deviceready",onDeviceReady,false);
+function onDeviceReady() {
+    pictureSource=navigator.camera.PictureSourceType;
+    destinationType=navigator.camera.DestinationType;
+}
 
-  function onDeviceReady() {
-      pictureSource=navigator.camera.PictureSourceType;
-      destinationType=navigator.camera.DestinationType;
-  }
+function onPhotoDataSuccess(imageData) {
+  var smallImage = document.getElementById('smallImage');
+  smallImage.style.display = 'block';
+  smallImage.src = "data:image/jpeg;base64," + imageData;
+}
 
-  function onPhotoDataSuccess(imageData) {
-    var smallImage = document.getElementById('smallImage');
-    smallImage.style.display = 'block';
-    smallImage.src = "data:image/jpeg;base64," + imageData;
-  }
+function capturePhoto() {
+  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+    destinationType: destinationType.DATA_URL });
+}
 
-  function capturePhoto() {
-    navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
-      destinationType: destinationType.DATA_URL });
-  }
+function onFail(message) {
+  alert('Failed because: ' + message);
+}
 
-  function onFail(message) {
-    alert('Failed because: ' + message);
-  }
-});
 
